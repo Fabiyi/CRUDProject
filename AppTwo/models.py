39e6e8ddc12ab078c30a834c1f9f65c2ff9517cf
Fabiyi <fabiyidanieldamilola@gmail.com>
2024-12-24 21:38:40 +0100

    first commit

---
+++ AppTwo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField()
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return self.name
